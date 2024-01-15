e5e6f36af1ee0edaf7ef9222aae1a233f5e874b4
tannal <tannal2409@gmail.com>
2024-01-15 09:30:25 +0800

    init

---
+++ db.go
@@ -0,0 +1,17 @@
+package kakapo
+
+func Open() {
+	panic("not implemented")
+}
+
+func Get() {
+	panic("not implemented")
+}
+
+func Put() {
+	panic("not implemented")
+}
+
+func Scan() {
+	// panic("not implemented")
+}
---
+++ db_test.go
@@ -0,0 +1,19 @@
+package kakapo
+
+import "testing"
+
+func TestOpen(t *testing.T) {
+	// Open()
+}
+
+func TestSet(t *testing.T) {
+	// Set()
+}
+
+func TestPut(t *testing.T) {
+	// Put()
+}
+
+func TestScan(t *testing.T) {
+	Scan()
+}
