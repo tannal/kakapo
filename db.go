bbfffd6b23e74b4da5ad60c32a531773fa9e17a0
tannal <tannal2409@gmail.com>
2024-02-17 14:19:52 +0800

    kakapo version1 single file storage sequential scan

--- db.go
+++ db.go
@@ -1,17 +1,166 @@
 package kakapo
 
-func Open() {
-	panic("not implemented")
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+
+	"github.com/google/btree"
+)
+
+type DB struct {
+	btree      *btree.BTree
+	max        int
+	activeFile os.File
+	walFile    os.File
 }
 
-func Get() {
-	panic("not implemented")
+type node struct {
+	Key string
+	Val string
 }
 
-func Put() {
-	panic("not implemented")
+func (n node) toJson() (string, error) {
+	jsonData, err := json.Marshal(n)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonData), nil
 }
 
-func Scan() {
-	// panic("not implemented")
+func (k node) Less(item btree.Item) bool {
+	otherKey := item.(node)
+	return k.Key < otherKey.Key
+}
+
+func Open() DB {
+	btree := btree.New(2)
+
+	file, err := os.OpenFile("data.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	walfile, err := os.OpenFile("wal.json", os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	// read wal file and replay changes
+	walfile.Seek(0, 0)
+	reader := bufio.NewReader(walfile)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		var n node
+		err = json.Unmarshal([]byte(line), &n)
+		if err != nil {
+			panic(err)
+		}
+
+		btree.ReplaceOrInsert(n)
+	}
+
+	return DB{
+		btree:      btree,
+		max:        10,
+		activeFile: *file,
+		walFile:    *walfile,
+	}
+
+}
+
+func (db DB) Get(k string) string {
+	key1 := node{
+		Key: k,
+	}
+	item := db.btree.Get(key1)
+
+	// read from active file line by line
+	if item == nil {
+		db.activeFile.Seek(0, 0)
+		reader := bufio.NewReader(&db.activeFile)
+
+		var val = ""
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				break
+			}
+			var n node
+			err = json.Unmarshal([]byte(line), &n)
+			if err != nil {
+				panic(err)
+			}
+			if n.Key == k {
+				val = n.Val
+			}
+		}
+		return val
+	}
+	return item.(node).Val
+
+}
+
+func (db DB) Put(k string, v string) {
+	key := node{
+		Key: k,
+		Val: v,
+	}
+
+	// write to wal file
+	jsonData, err := key.toJson()
+	if err != nil {
+		panic(err)
+	}
+	db.walFile.WriteString(jsonData + "\n")
+
+	db.btree.ReplaceOrInsert(key)
+
+	if db.btree.Len() >= db.max {
+		node2 := db.btree.DeleteMin()
+		for node2 != nil {
+			// write json to active file line 1
+
+			jsonData, err := node2.(node).toJson()
+			if err != nil {
+				panic(err)
+			}
+
+			// write to the begginning of the file
+			db.activeFile.WriteString(jsonData + "\n")
+			node2 = db.btree.DeleteMin()
+		}
+		db.walFile.Truncate(0)
+	}
+}
+
+func (db DB) Scan() []node {
+	var pairs []node
+	db.btree.Ascend(func(item btree.Item) bool {
+		pairs = append(pairs, item.(node))
+		return true
+	})
+	return pairs
+}
+
+// close
+func (db DB) Close() {
+	db.activeFile.Seek(2, 0)
+	node2 := db.btree.DeleteMin()
+	for node2 != nil {
+		// write json to active file line 1
+
+		jsonData, err := node2.(node).toJson()
+		if err != nil {
+			panic(err)
+		}
+
+		// write to the begginning of the file
+		db.activeFile.WriteString(jsonData + "\n")
+		node2 = db.btree.DeleteMin()
+	}
+	db.activeFile.Close()
+	db.walFile.Close()
 }
--- db_test.go
+++ db_test.go
@@ -1,24 +1,139 @@
 package kakapo
 
 import (
+	"strconv"
 	"testing"
 
-	"github.com/google/btree"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestOpen(t *testing.T) {
-	// Open()
-	btree.New(2)
+func TestPutGetInMemory(t *testing.T) {
+	db := Open()
+
+	db.Put("1", "2")
+	val := db.Get("1")
+
+	assert.Equal(t, "2", val)
+
+	db.Put("1", "3")
+
+	val = db.Get("1")
+
+	assert.Equal(t, "3", val)
 }
 
-func TestSet(t *testing.T) {
-	// Set()
+func TestScanInMemory(t *testing.T) {
+	db := Open()
+
+	db.Put("1", "2")
+	val := db.Get("1")
+
+	assert.Equal(t, "2", val)
+
+	db.Put("1", "3")
+
+	val = db.Get("1")
+
+	assert.Equal(t, "3", val)
+
+	// get the scan result and assert it
+	pairs := db.Scan()
+
+	t.Log("pairs", pairs)
+	assert.Equal(t, 1, len(pairs))
+
 }
 
-func TestPut(t *testing.T) {
-	// Put()
+func TestFlushToDisk(t *testing.T) {
+	db := Open()
+
+	// put ten key value pair
+	for i := 0; i < 10; i++ {
+		db.Put(strconv.Itoa(i), strconv.Itoa(i*2))
+	}
+
+	// get the scan result and assert it
+	pairs := db.Scan()
+
+	t.Log("pairs", pairs)
+	assert.Equal(t, 10, len(pairs))
+
 }
 
-func TestScan(t *testing.T) {
-	Scan()
+func TestSearchOnDisk(t *testing.T) {
+	db := Open()
+
+	// put ten key vialue pair
+	for i := 0; i < 10; i++ {
+		db.Put(strconv.Itoa(i), strconv.Itoa(i*2))
+	}
+
+	// get the scan result and assert it
+	pairs := db.Scan()
+
+	t.Log("pairs", pairs)
+
+	val := db.Get("1")
+
+	t.Log("val", val)
+
+	assert.Equal(t, "2", val)
+
+}
+
+func TestChineseString(t *testing.T) {
+	db := Open()
+
+	db.Put("1", "你好")
+	val := db.Get("1")
+
+	assert.Equal(t, "你好", val)
+
+	db.Put("1", "再见")
+
+	val = db.Get("1")
+
+	assert.Equal(t, "再见", val)
+
+	db.Close()
+}
+
+func TestCrash(t *testing.T) {
+	db := Open()
+
+	// put ten key vialue pair
+	for i := 0; i < 9; i++ {
+		db.Put(strconv.Itoa(i), strconv.Itoa(i*2))
+	}
+
+	// get the scan result and assert it
+	pairs := db.Scan()
+
+	t.Log("pairs", pairs)
+
+	val := db.Get("1")
+
+	t.Log("val", val)
+
+	assert.Equal(t, "2", val)
+
+	db.Close()
+
+	db = Open()
+
+	val = db.Get("1")
+
+	t.Log("val", val)
+
+	assert.Equal(t, "2", val)
+
+	db.Put("10", "20")
+
+	val = db.Get("10")
+
+	t.Log("val", val)
+
+	assert.Equal(t, "20", val)
+
+	db.Close()
 }
